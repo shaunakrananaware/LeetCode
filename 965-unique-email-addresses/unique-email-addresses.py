b8d525153c84dc2c5f1dd324c11c3472ce504eac
Shaunak Rananaware <113698409+shaunakrananaware@users.noreply.github.com>
2024-10-01 15:26:52 +0530

    Time: 51 ms (77.12%) | Memory: 16.7 MB (58.26%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -1,30 +1,10 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        unique_emails = {}
-        unique_email_count = 0
+        unique_emails = set()
 
-        for idx in range(len(emails)):
-            local_email = ''
-            
-            jdx = 0
-            while emails[idx][jdx] != '@':
-                if emails[idx][jdx] == '+':
-                    while emails[idx][jdx] != '@':
-                        jdx += 1
-                    break
+        for email in emails:
+            local, domain = email.split('@')
+            local = local.split('+')[0].replace('.', '')
+            unique_emails.add(local + '@' + domain)
 
-                if emails[idx][jdx] == '.':
-                    jdx += 1
-                    continue
-
-                local_email += emails[idx][jdx]
-                jdx += 1
-            
-            final_email = f'{local_email}{emails[idx][jdx:]}'
-            
-            if final_email not in unique_emails: 
-                unique_emails[final_email] = 0
-                unique_email_count += 1
-        
-        return unique_email_count
-
+        return len(unique_emails)
