369b518fa3d53ca89fc8a6a0f915c59bc425f4d6
Shaunak Rananaware <113698409+shaunakrananaware@users.noreply.github.com>
2024-10-02 18:31:38 +0530

    Time: 246 ms (85.83%) | Memory: 26.7 MB (35.81%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,21 +1,10 @@
 class Solution:
-    
-    @staticmethod
-    def binarySearch(nums: List[int], key: int) -> bool:
-        left = 0
-        right = len(nums)-1
-
-        while left <= right:
-            mid = (right+left) // 2
-            if key == nums[mid]: return True
-            if key < nums[mid]:
-                right = mid - 1
-                continue
-            left = mid+1
-
-        return False
-
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        num_set = set(nums)
+        arr = []
 
-        return [i for i in range(1, len(nums)+1) if not Solution.binarySearch(nums, i)]
+        for idx in range(1, len(nums)+1):
+            if idx not in num_set:
+                arr.append(idx)
+
+        return arr
